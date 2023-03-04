b99374f4d7ff8e732c5ee80b024ac7e539582791
woosung <jungyh0528@gmail.com>
2023-03-04 22:38:48 +0900

    fix : fix lint for dom-array-methods

--- dom-array-methods/script.js
+++ dom-array-methods/script.js
@@ -1,46 +1,51 @@
-const addUserButton = document.getElementById("add-user");
-const doubleMoneyButton = document.getElementById("double-money");
-const showMillionaireButton = document.getElementById("show-millionaire");
-const sortRichestButton = document.getElementById("sort-richest");
-const sumWealthButton = document.getElementById("sum-wealth");
-const personContainer = document.querySelector(".person-container");
+const addUserButton = document.getElementById('add-user');
+const doubleMoneyButton = document.getElementById('double-money');
+const showMillionaireButton = document.getElementById('show-millionaire');
+const sortRichestButton = document.getElementById('sort-richest');
+const sumWealthButton = document.getElementById('sum-wealth');
+const personContainer = document.querySelector('.person-container');
 
-const createPerson = ({first, last}) => {
-  const newPerson = document.createElement("div");
-  newPerson.classList.add("person");
-  const newName = document.createElement("h3");
-  newName.classList.add("name");
+const createPerson = ({ first, last }) => {
+  const newPerson = document.createElement('div');
+  newPerson.classList.add('person');
+
+  const newName = document.createElement('h3');
+  newName.classList.add('name');
   newName.textContent = `${first} ${last}`;
-  const newWealth = document.createElement("h3");
-  newWealth.classList.add("wealth");
+
+  const newWealth = document.createElement('h3');
+  newWealth.classList.add('wealth');
   const randomWealth = Math.floor(Math.random() * 1_000_000);
   newWealth.textContent = `$${randomWealth.toLocaleString('en-US')}.00`;
+
   newPerson.appendChild(newName);
   newPerson.appendChild(newWealth);
 
   personContainer.appendChild(newPerson);
-}
+};
 
 const createSumArea = (sum) => {
-  const newSumText = document.createElement("h3");
-  newSumText.classList.add("sum");
-  newSumText.textContent = "Total Wealth:";
-  const newSum = document.createElement("strong");
-  newSum.textContent = `$${sum.toLocaleString('en-US')}.00`
+  const newSumText = document.createElement('h3');
+  newSumText.classList.add('sum');
+  newSumText.textContent = 'Total Wealth:';
+
+  const newSum = document.createElement('strong');
+  newSum.textContent = `$${sum.toLocaleString('en-US')}.00`;
+
   newSumText.appendChild(newSum);
   personContainer.appendChild(newSumText);
-}
+};
 
 const clearSumArea = () => {
-  const sumAreas = Array.from(personContainer.querySelectorAll(".sum"));
-  sumAreas.forEach(sumArea => {sumArea.remove()})
-}
+  const sumAreas = Array.from(personContainer.querySelectorAll('.sum'));
+  sumAreas.forEach((sumArea) => { sumArea.remove(); });
+};
 
 const addRandomUser = async () => {
-  const res = await fetch("https://randomuser.me/api/");
+  const res = await fetch('https://randomuser.me/api/');
   const data = await res.json();
   createPerson(data.results[0].name);
-}
+};
 
 const createInitialPeople = () => {
   const promises = [];
@@ -48,61 +53,58 @@ const createInitialPeople = () => {
   promises.push(addRandomUser());
   promises.push(addRandomUser());
   Promise.all(promises);
-}
+};
 
 const getWealthNumber = (textWealth) => {
-  const wealthNumberString = textWealth.split("$")[1].split(".")[0].replace(/\D/g,'');
+  const wealthNumberString = textWealth.split('$')[1].split('.')[0].replace(/\D/g, '');
   return Number(wealthNumberString);
-}
+};
 
 const handleClickAddUserButton = () => {
   addRandomUser();
-}
+};
 
 const handleClickDoubleMoneyButton = () => {
-  const people = Array.from(personContainer.querySelectorAll(".person"));
-  people.map(person => {
+  const people = Array.from(personContainer.querySelectorAll('.person'));
+  people.map((person) => {
     const doubledPerson = person;
-    const wealthText = person.querySelector(".wealth");
+    const wealthText = doubledPerson.querySelector('.wealth');
     const wealth = getWealthNumber(wealthText.textContent);
-    const doubleWealth = wealth * 2
+    const doubleWealth = wealth * 2;
     wealthText.textContent = `$${doubleWealth.toLocaleString('en-US')}.00`;
     return doubledPerson;
-  }).forEach(person => {
+  }).forEach((person) => {
     personContainer.appendChild(person);
-  })
-}
+  });
+};
 
 const handleClickShowMillionaireButton = () => {
-  const people = Array.from(personContainer.querySelectorAll(".person"));
-  people.filter(person => {
-    const wealthText = person.querySelector(".wealth");
+  const people = Array.from(personContainer.querySelectorAll('.person'));
+  people.filter((person) => {
+    const wealthText = person.querySelector('.wealth');
     const wealth = getWealthNumber(wealthText.textContent);
     return wealth < 1_000_000;
-  }).forEach(person => {
+  }).forEach((person) => {
     person.remove();
-  })
-}
+  });
+};
 
 const handleClickSortRichestButton = () => {
-  const people = Array.from(personContainer.querySelectorAll(".person"));
+  const people = Array.from(personContainer.querySelectorAll('.person'));
   people.sort((a, b) => {
-    const wealthA = getWealthNumber(a.querySelector(".wealth").textContent);
-    const wealthB = getWealthNumber(b.querySelector(".wealth").textContent);
+    const wealthA = getWealthNumber(a.querySelector('.wealth').textContent);
+    const wealthB = getWealthNumber(b.querySelector('.wealth').textContent);
     return wealthB - wealthA;
-  }).forEach(person => {
+  }).forEach((person) => {
     personContainer.appendChild(person);
-  })
-}
+  });
+};
 
 const handleClickSumWealthButton = () => {
-  const people = Array.from(personContainer.querySelectorAll(".person"));
-  const total = people.reduce((accumulator, person) => {
-    return accumulator + getWealthNumber(person.querySelector(".wealth").textContent);
-  }, 0);
+  const people = Array.from(personContainer.querySelectorAll('.person'));
+  const total = people.reduce((accumulator, person) => accumulator + getWealthNumber(person.querySelector('.wealth').textContent), 0);
   createSumArea(total);
-}
-
+};
 
 window.addEventListener('load', createInitialPeople);
 
